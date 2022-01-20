a87484cba5942c21126784bf10fb48bc3beaa942
Hollowlake <70709767+hollowlake@users.noreply.github.com>
2022-01-20 08:28:17 -0700

    Added comments for readability

--- speed.py
+++ speed.py
@@ -6,37 +6,44 @@ import directories
 import os
 import folium
 
+# This Python scripts conducts a speedtest of the internet. 
+# It takes the result and stores it in a database, prints the results to console, and makes a map.
 
+
+# This function is the actual test. 
 def test():
-    
+
+    # Starting the speedtest.   
     print("\nInitializing speedtest...")
-    # Speed Test Variable for shortening
     test = speedtest.Speedtest()
 
-    #locates the best server
+    # locating the best server.
     print("Finding the best server...")
     best = test.get_best_server()
 
-    # Takes that information and pretty formats it for the JSON file
+    # The server information is in JSON format. Info takes the information, intel loads it back into python.
     info = json.dumps(best, indent=4, sort_keys=True)
     intel = json.loads(info)
 
-    # Conducts download test
+    # Conducts download test.
     print("Testing Download Speed...")
     download = test.download()
 
-    # Conducts upload test
+    # Conducts upload test.
     print("Testing Upload Speed...")
     upload = test.upload()
 
-    # Conducts ping test
+    # Conducts ping test.
     print("Testing Ping Time...\n\n")
     ping = test.results.ping
     
+    # Sends the relevant information to the next function.
     information(intel, download, upload, ping)
 
+# This function stores all the information for use in other functions. 
 def information(intel, download, upload, ping):
     
+    # This pulls all the information into keys that can be used later
     dread = (f'{download / 1024 / 1024:.2f} Mbps')
     uread = (f'{upload / 1024 / 1024:.2f} Mbps')
     pread = (f'{ping:.2f}ms')
@@ -47,8 +54,8 @@ def information(intel, download, upload, ping):
     name = intel['name']
     sponsor = intel['sponsor']
     timestr = time.strftime("%Y%m%d-%H%M%S")
-
     
+    # Readfile is for a not yet created function.
     readfile = (f'Download: {dread}\n\
     Upload: {uread}\n\
     Ping: {pread}\n\
@@ -59,22 +66,27 @@ def information(intel, download, upload, ping):
     Name: {name}\n\
     Sponsor: {sponsor}')
 
+    # Prints the information for the user to see in console. 
     print(f'Download Speed: {dread}')
     print(f'Upload Speed: {uread}')
     print(f'Ping: {pread}\n\n')
     print("Sending information to database and creating map...")
         
-    dbinput(intel, dread, uread, pread, d, host, lat, lon, name, sponsor, timestr)
+    # Sends the relevant information to the next functions.     
+    dbinput(dread, uread, pread, d, host, lat, lon, name, sponsor, timestr)
     mapcreation(lat, lon, dread, uread, pread, timestr)
     
-def dbinput(intel, dread, uread, pread, d, host, lat, lon, name, sponsor, timestr):
+# This function takes the passed information and uploads it to a database for future reference.     
+def dbinput(dread, uread, pread, d, host, lat, lon, name, sponsor, timestr):
     
+    # Connects to the database and establishes a cursor
     connect = sqlite3.connect('speedtest.db')
     cursor = connect.cursor()
 
-    # Prints formated results to console   
+    # Query to insert the information into the corresponding table   
     insertQuery = """INSERT INTO results (download, upload, ping, d, host, lat, lon, name, sponsor, time) VALUES (?,?,?,?,?,?,?,?,?,?)"""
         
+    # Pushes the information to the database    
     with connect as connection:
         
         cursor.execute(insertQuery, [dread, uread, pread, d, host, lat, lon, name, sponsor, timestr])
@@ -84,18 +96,21 @@ def dbinput(intel, dread, uread, pread, d, host, lat, lon, name, sponsor, timest
    
 def mapcreation(lat, lon, dread, uread, pread, timestr): 
       
+    # Uses a hidden directory file to tell the script where to put the map and what to call it  
     completemap = os.path.join(directories.directory, timestr+'.html') 
+    
+    # Coordinates passed from a previous function for the map
     coordinates = (lat, lon)
 
+    # Map configuration, makes popup with results
     map = folium.Map(coordinates, zoom_start=15)
-
-    # Map popup configuration, makes popup with results
     overall = (f"Download: {dread} / Upload: {uread} / Ping: {pread}")
     popup = folium.Popup(overall, max_width=400,min_width=100)
     folium.Marker(coordinates, popup = popup).add_to(map)
 
     # Saves map as html file in the maps folder
     map.save(completemap)
+    
     print('Map created.')
         
 test()
