ef2a112ad18c648acdfdc1eb605cd4162cd33b3f
bellandari <bellandari@feartheaquapickle.com>
2022-12-02 17:07:51 -0700

    Removed map

--- clickspeed.py
+++ clickspeed.py
@@ -59,59 +59,7 @@ def runtest(run):
             headers = ['Tag', 'Lat', 'Lon', 'Download', 'Upload', 'Ping', 'Name', 'Sponsor', 'Date', 'Time']
             click.echo("")
             click.echo(tabulate(result, headers=headers))
-            
-            for x in result:
-                newresult = x
-            
-            lookupmap(newresult)
-
-def lookupmap(newresult):
-    
-    # Defines variables as pulled from the database 
-    tag = newresult[0]     
-    lat = newresult[1]
-    lon = newresult[2]
-    download = newresult[3]
-    upload = newresult[4]
-    ping = newresult[5]
-    name = newresult[6]
-    sponsor = newresult[7]
-    date = newresult[8]
-    
-    coordinates = (lat, lon)      
-    
-    # Establishes map
-    datamap = folium.Map(coordinates, zoom_start=15)
-
-    # Formatting for Map Popup
-    results = (f"\
-        <b>Download:</b> {download}<br> \
-        <b>Upload:</b> {upload}<br> \
-        <b>Ping:</b> {ping}<br> \
-        <b>Location:</b> {name}<br>\
-        <b>Sponsor:</b> {sponsor}<br>\
-        <b>Tag:</b> {tag}<br>\
-        <b>Date:</b>{date}")
-    
-    # Popup configuration
-    popup = folium.Popup(results, max_width=600, min_width=300)
-    folium.Marker(coordinates, popup = popup).add_to(datamap)
-
-    # Saves map as html file in the maps folder
-    datamap.save('results.html')
-    
-    # Deploys window with map
-    webbrowser.get("windows-default").open('results.html')
-
-    click.echo("")
-    click.echo(f"Map & Results for Speedtest {tag} Launched. ")
-    
-    # Delay to prevent system deleting map before it is opened
-    time.sleep(2)
-    
-    # Deletes map from system
-    os.remove(f'results.html')
-                   
+                               
 def table():
     
     with connect as connection:
@@ -159,60 +107,7 @@ def dbinput(best, dread, uread, pread, tag):
             cursor.execute(insertQuery, [tag, dread, uread, pread, best['d'], best['host'], best['lat'], best['lon'], best['name'], best['sponsor'], date, timestr])
             connection.commit()    
             
-        click.echo("Information uploaded to database, successfully!")
-        createmap()
-            
-def createmap():
-        
-    searchcommand = "SELECT lat, lon, download, upload, ping, name, sponsor FROM (SELECT * FROM results ORDER BY date DESC, time DESC) LIMIT 1"
-
-    with connect as connection:
-            
-        cursor.execute(searchcommand)
-        result = cursor.fetchall()        
-            
-    # For loop places results into a dictionary        
-    for x in result:
-        newresult = x
-                    
-    # Defines variables as pulled from the database      
-    lat = newresult[0]
-    lon = newresult[1]
-    download = newresult[2]
-    upload = newresult[3]
-    ping = newresult[4]
-    name = newresult[5]
-    sponsor = newresult[6]
-        
-    coordinates = (lat, lon)      
-
-    # Establishes map
-    datamap = folium.Map(coordinates, zoom_start=15)
-
-    # Formatting for Map Popup
-    results = (f"<b>Download:</b> {download}<br> \
-        <b>Upload:</b> {upload}<br> \
-        <b>Ping:</b> {ping}<br> \
-        <b>Location:</b> {name}<br>\
-        <b>Sponsor:</b> {sponsor}")
-
-    # Popup configuration
-    popup = folium.Popup(results, max_width=600, min_width=300)
-    folium.Marker(coordinates, popup = popup).add_to(datamap)
-
-    # Saves map as html file in the maps folder
-    datamap.save('map.html')
-
-    # Deploys window with map
-    webbrowser.get("windows-default").open('map.html')
-
-    # Delay to prevent system deleting map before it is opened
-    time.sleep(2)
-
-    # Deletes map from system
-    os.remove('map.html')
-
-    click.echo("Map launched in browser!\n")     
+        click.echo("Information uploaded to database, successfully!")       
                        
 while True:
     
